15fbd68fbd2631d9fbb7ae5c21eab05a0382dd1a
student <student@158.197.35.79>
2019-10-15 09:54:24 +0200

    dds

--- prezencka/src/main/java/sk/upjs/prezencka/App.java
+++ prezencka/src/main/java/sk/upjs/prezencka/App.java
@@ -1,13 +1,24 @@
 package sk.upjs.prezencka;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SubjectEdit.fxml"));
+		Parent parent =fxmlLoader.load();
+		
+		Scene scene = new Scene(parent);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
